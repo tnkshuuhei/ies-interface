1f54a1a8a5a2b1dc279a38c1bacdaaea9edabb94
shuhei tanaka <shuhei46491@gmail.com>
2024-10-19 01:00:50 +0700

    add pkgs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ipfs.io", "github.com"],
+    domains: ["ipfs.io", "github.com", "picsum.photos"],
   },
   reactStrictMode: true,
   webpack: (config) => {
