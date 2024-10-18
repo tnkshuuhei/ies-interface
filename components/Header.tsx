1529cd2209ab6cce20864791467d655b3aaaf795
shuhei tanaka <shuhei46491@gmail.com>
2024-10-19 01:50:25 +0700

    fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,47 +12,14 @@ const Header = () => {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link href="/" className="flex items-center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              width={100}
-              height={24}
+              src="/ies-logo.svg"
+              alt="Logo"
+              width={50}
+              height={50}
               priority
             />
           </Link>
-          <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
-          >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <Link
-                  href="/"
-                  className="block py-2 pr-4 pl-3 text-gray-700 bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/user"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
-                >
-                  Account
-                </Link>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/tnkshuuhei"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 "
-                >
-                  Github
-                </a>
-              </li>
-            </ul>
-          </div>
+
           <div className="flex items-center lg:order-2 gap-4">
             <ConnectButton />
           </div>
