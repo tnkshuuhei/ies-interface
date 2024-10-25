475a73b9f6baf73f4f775080de54d446768693a9
shuhei tanaka <shuhei46491@gmail.com>
2024-10-25 20:03:51 +0700

    style: add skelton

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { gql, request } from "graphql-request";
 import Link from "next/link";
 
 import ProjectCard from "@/components/ProjectCard";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import { Profiles } from "@/utils/types";
 
@@ -25,7 +27,23 @@ const query = gql`
     }
   }
 `;
+const ProjectCardSkeleton = () => {
+  return (
+    <div className="w-lg rounded-[15px] bg-white cursor-pointer shadow-lg">
+      <AspectRatio ratio={18 / 9} className="bg-muted">
+        <Skeleton className="w-full h-full rounded-md" />
+      </AspectRatio>
 
+      <div className="flex flex-col px-4 py-2 gap-2">
+        <div className="flex flex-row items-center gap-2">
+          <Skeleton className="h-[26px] w-3/4" />
+        </div>
+        <Skeleton className="h-[18px] w-full" />
+        <Skeleton className="h-[22px] w-1/2" />
+      </div>
+    </div>
+  );
+};
 export default function Home() {
   const {
     data: profiles,
@@ -43,6 +61,18 @@ export default function Home() {
     },
     refetchInterval: 10000,
   });
+
+  if (isLoadingProfiles) {
+    return (
+      <main className="min-h-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-12">
+          {Array.from({ length: 12 }).map((_, index) => (
+            <ProjectCardSkeleton key={index} />
+          ))}
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="min-h-screen">
