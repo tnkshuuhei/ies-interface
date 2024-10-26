ff893e5ae23ab392eb9bd6c935b28c0217443c4f
shuhei tanaka <shuhei46491@gmail.com>
2024-10-26 15:01:49 +0700

    chore: approve 1000 token

--- app/create/[id]/page.tsx
+++ app/create/[id]/page.tsx
@@ -64,6 +64,40 @@ const formSchema = z.object({
 
 type FormData = z.infer<typeof formSchema>;
 
+const defaultValues: FormData = {
+  name: "# Protcol Guild Impact Report.",
+  description:
+    "## Protocol Guild has distributed more than 33M dollars to Ethereum Core Developers and Researchers.",
+  contributors: [
+    { address: "0xc3593524E2744E547f013E17E6b0776Bc27Fc614" },
+    {
+      address: "0x63b1EfC5602C0023BBb373F2350Cf34c2E5F8669",
+    },
+  ],
+  links: [{ url: "https://dune.com/protocolguild/protocol-guild" }],
+  roles: [
+    {
+      parentHatId:
+        "16796047185010987922379060937955525549013567220926743064122515022413824",
+      name: "Developer",
+      description: "Developers are responsible for building the protocol.",
+      wearers: ["0xc3593524E2744E547f013E17E6b0776Bc27Fc614" as `0x${string}`],
+      imageUrl: "ipfs://QmRZ9ULzLKC1uzAvLyxAAhYoXyQMS413zPviGLu6vG4Bzw",
+    },
+    {
+      parentHatId:
+        "16796047185010987922379060937955525549013567220926743064122515022413824",
+      name: "Researcher",
+      description: "Researchers are responsible for researching the protocol.",
+      wearers: [
+        "0xc3593524E2744E547f013E17E6b0776Bc27Fc614",
+        "0x63b1EfC5602C0023BBb373F2350Cf34c2E5F8669",
+      ],
+      imageUrl: "ipfs://QmTRGCnTfwHhyr64aSNZqpP68ABFNQu9W9TJZEo4vL3FRu",
+    },
+  ],
+};
+
 export default function Register({ params }: { params: { id: string } }) {
   const [imageName, setImageName] = useState<string | null>(null);
   const [image, setImage] = useState<string | undefined>(undefined);
@@ -141,21 +175,22 @@ export default function Register({ params }: { params: { id: string } }) {
 
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      description: "",
-      contributors: [{ address: account.address as `0x${string}` }],
-      links: [{ url: "" }],
-      roles: [
-        {
-          parentHatId: params.id,
-          name: "",
-          description: "",
-          wearers: [account.address as `0x${string}`],
-          imageUrl: "",
-        },
-      ],
-    },
+    // defaultValues: {
+    //   name: "",
+    //   description: "",
+    //   contributors: [{ address: account.address as `0x${string}` }],
+    //   links: [{ url: "" }],
+    //   roles: [
+    //     {
+    //       parentHatId: params.id,
+    //       name: "",
+    //       description: "",
+    //       wearers: [account.address as `0x${string}`],
+    //       imageUrl: "",
+    //     },
+    //   ],
+    // },
+    defaultValues: defaultValues,
   });
 
   const {
@@ -319,7 +354,7 @@ export default function Register({ params }: { params: { id: string } }) {
 
       const encoded = await encodedRoles(rolesCopy);
 
-      await approve(ies.address, "100");
+      await approve(ies.address, "1000");
 
       const mappingContributors = data.contributors.map(
         (contributor) => contributor.address
